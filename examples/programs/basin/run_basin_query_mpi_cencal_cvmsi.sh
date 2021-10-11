c9df04144baf6519de0caf3da56373672b10f449
Mei-Hui Su <mei@MeiPro.local>
2021-10-11 14:22:43 -0700

    rework basin query with mpi

--- examples/programs/basin/run_basin_query_mpi_cencal_cvmsi.sh
+++ examples/programs/basin/run_basin_query_mpi_cencal_cvmsi.sh
@@ -11,12 +11,12 @@ source $UCVM_INSTALL_PATH/conf/ucvm_env.sh
 
 BIN_DIR=${UCVM_INSTALL_PATH}/bin
 CONF_DIR=${UCVM_INSTALL_PATH}/conf
-TEST=basin_query_mpi_cvms5
+TEST=basin_query_mpi_cencal_cvmsi
 
 expect=$(mktemp) || exit 1
 result=$(mktemp) || (trap 'rm -f "$expect"'; exit 1)
 
-salloc -Q --ntasks=2 --time=00:10:00 srun -Q --ntasks=2 -o ${TEST}.srun.out ${BIN_DIR}/basin_query_mpi -b ${TEST}.simple -f ${CONF_DIR}/ucvm.conf -m cvms5 -i 20 -v 2500 -l 35.0,-122.5 -s 0.1 -x 16 -y 11
+salloc -Q --ntasks=2 --time=00:10:00 srun -Q --ntasks=2 -o ${TEST}.srun.out ${BIN_DIR}/basin_query_mpi -b ${TEST}.simple -f ${CONF_DIR}/ucvm.conf -m cencal,cvmsi -i 20 -v 2500 -l 35.0,-122.5 -s 0.1 -x 16 -y 11
 
 od ${TEST}.simple > $result 2>&1
 
@@ -33,7 +33,7 @@ cat > $expect << EOF_EXPECTED_RESULT
 0001300
 EOF_EXPECTED_RESULT
 
-echo "Running examples_programs_basin basin_query_mpi_cvms5"
+echo "Running examples_programs_basin basin_query_mpi_cencal_cvmsi"
 if diff $result $expect > /dev/null 2>&1
 then
   echo [SUCCESS]
