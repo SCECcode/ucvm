265fe27e97d41a9fdc1f79441021c8d670071efd
Mei-Hui Su <mei@MeiPro.local>
2022-01-26 16:01:13 -0800

    add building of cvmhlabn

---
+++ .github/scripts/ucvm-build-cvmhlabn.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+tmp=`uname -s`
+
+if [ $tmp == 'Darwin' ]; then
+## make sure have automake/aclocal
+  brew install automake
+fi
+
+mkdir $UCVM_INSTALL_PATH
+
+##["cvms5", "cca", "cs173", "cs173h", "cvms", "cvmsi", "cencal", "cvmh", "albacore", "cvlsu", "ivlsu", "wfcvm","cvmhlabn"]
+
+cd $UCVM_SRC_PATH/largefiles
+./get_largefiles.py << EOF
+n
+n
+n
+n
+n
+n
+n
+n
+n
+n
+n
+n
+y
+EOF
+
+if [ $tmp != 'Darwin' ]; then
+  cd $UCVM_SRC_PATH/largefiles; ./check_largefiles_md5.py
+fi
+cd $UCVM_SRC_PATH/largefiles; ./stage_largefiles.py
+
+cd $UCVM_SRC_PATH
+./ucvm_setup.py -d -a << EOF &> ucvm_setup_install.log
+$UCVM_INSTALL_PATH
+EOF
+
+more ucvm_setup_install.log
+
