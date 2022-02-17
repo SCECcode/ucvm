3f0325cc2323ec54dc1c631ad563f54fb41fcbd7
Mei-Hui Su <mei@MeiPro.local>
2022-02-16 23:59:17 -0800

    fix logic

--- src/ucvm/ucvm_model_plugin.c
+++ src/ucvm/ucvm_model_plugin.c
@@ -361,7 +361,7 @@ int ucvm_plugin_model_query(int id, ucvm_ctype_t cmode, int n, ucvm_point_t *pnt
 	}
 
 /* NOT sure when this came in.. */
-        if((pptr->model_setparam == 0) || (*(pptr->model_setparam))(id, UCVM_PARAM_QUERY_MODE, cmode ) != 0) {
+        if((pptr->model_setparam != 0) || (*(pptr->model_setparam))(id, UCVM_PARAM_QUERY_MODE, cmode ) != 0) {
                 fprintf(stderr, "Failed to set query mode flag for model\n");
                 return UCVM_CODE_ERROR;
         }
