e16158ab73772114c1efccb03b180476e255ff9c
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2022-03-09 07:59:44 +0000

    add cencal cvms scripts

---
+++ .github/scripts/run-build-cencal-cvms.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#sudo apt-get install gfortran
+mkdir $UCVM_INSTALL_PATH
+
+##["cvms5", "cca", "cs173", "cs173h", "cvms", "cvmsi", "cencal", "cvmh", "albacore", "cvlsu", "ivlsu", "wfcvm"]
+
+cd $UCVM_SRC_PATH/largefiles
+./get_largefiles.py << EOF
+n
+n
+n
+n
+y
+n
+y
+n
+n
+n
+n
+n
+EOF
+
+if [ $tmp != 'Darwin' ]; then
+  cd $UCVM_SRC_PATH/largefiles; ./check_largefiles_md5.py
+fi
+cd $UCVM_SRC_PATH/largefiles; ./stage_largefiles.py
+
+cd $UCVM_SRC_PATH
+./ucvm_setup.py -d -a << EOF &> ucvm_setup_install.log
+$UCVM_INSTALL_PATH
+EOF
+
