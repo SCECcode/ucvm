95cd3afbd1a7be1270944d1ef2948d0cbc2103db
Mei-Hui Su <mei@MeiPro.local>
2022-03-14 13:52:47 -0700

    remove duplicate

--- src/ucvm/ucvm.c
+++ src/ucvm/ucvm.c
@@ -1209,14 +1209,6 @@ int ucvm_get_resources(ucvm_resource_t *res, int *len)
   }
 #endif
 
-#ifdef _UCVM_ENABLE_WFCVM
-  if (ucvm_save_resource(UCVM_RESOURCE_MODEL, UCVM_MODEL_CRUSTAL,
-		     UCVM_MODEL_WFCVM, "", res, numinst++, *len) 
-      != UCVM_CODE_SUCCESS) {
-    return(UCVM_CODE_ERROR);
-  }
-#endif
-
 #ifdef _UCVM_ENABLE_CVMLT
   if (ucvm_save_resource(UCVM_RESOURCE_MODEL, UCVM_MODEL_CRUSTAL,
 		     UCVM_MODEL_CVMLT, "", res, numinst++, *len) 
