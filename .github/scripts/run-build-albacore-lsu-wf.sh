8b631f00210534a88adb7005bd348199a195252e
Philip Maechling <pjmaechling@users.noreply.github.com>
2022-03-16 15:12:36 -0700

    Update run-build-albacore-lsu-wf.sh
    added response to get large files to include both lsu models, but not to install wfcvm or labasin cvm

--- .github/scripts/run-build-albacore-lsu-wf.sh
+++ .github/scripts/run-build-albacore-lsu-wf.sh
@@ -3,7 +3,7 @@
 #sudo apt-get install gfortran
 mkdir $UCVM_INSTALL_PATH
 
-##["cvms5", "cca", "cs173", "cs173h", "cvms", "cvmsi", "cencal", "cvmh", "albacore", "cvlsu", "ivlsu", "wfcvm"]
+##["cvms5", "cca", "cs173", "cs173h", "cvms", "cvmsi", "cencal", "cvmh", "albacore", "cvlsu", "ivlsu", "wfcvm","cvmhlabn"]
 
 cd $UCVM_SRC_PATH/largefiles
 ./get_largefiles.py << EOF
@@ -15,6 +15,7 @@ n
 n
 n
 n
+y
 y
 y
 n
