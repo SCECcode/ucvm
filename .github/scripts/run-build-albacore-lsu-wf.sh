3a35883cdddf9095112d3e726c017e2a9a8622a3
Philip Maechling <pjmaechling@users.noreply.github.com>
2022-03-16 14:45:59 -0700

    Update run-build-albacore-lsu-wf.sh
    add wfcvm back into array. change y to n for final wfcvm

--- .github/scripts/run-build-albacore-lsu-wf.sh
+++ .github/scripts/run-build-albacore-lsu-wf.sh
@@ -3,7 +3,7 @@
 #sudo apt-get install gfortran
 mkdir $UCVM_INSTALL_PATH
 
-##["cvms5", "cca", "cs173", "cs173h", "cvms", "cvmsi", "cencal", "cvmh", "albacore", "cvlsu", "ivlsu"]
+##["cvms5", "cca", "cs173", "cs173h", "cvms", "cvmsi", "cencal", "cvmh", "albacore", "cvlsu", "ivlsu", "wfcvm"]
 
 cd $UCVM_SRC_PATH/largefiles
 ./get_largefiles.py << EOF
@@ -17,7 +17,7 @@ n
 n
 y
 y
-y
+n
 EOF
 
 if [ $tmp != 'Darwin' ]; then
