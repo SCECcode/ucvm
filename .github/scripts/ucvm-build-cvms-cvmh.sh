ad743696e0bcb6d7c23aaeac30cbebbcd34431da
Philip Maechling <pjmaechling@users.noreply.github.com>
2022-03-16 15:05:19 -0700

    Update ucvm-build-cvms-cvmh.sh
    Added 12th model into get_largefiles.py input lines to represent the labasinmodel

--- .github/scripts/ucvm-build-cvms-cvmh.sh
+++ .github/scripts/ucvm-build-cvms-cvmh.sh
@@ -3,7 +3,7 @@
 #sudo apt-get install gfortran
 mkdir $UCVM_INSTALL_PATH
 
-##["cvms5", "cca", "cs173", "cs173h", "cvms", "cvmsi", "cencal", "cvmh", "albacore", "cvlsu", "ivlsu"]
+##["cvms5", "cca", "cs173", "cs173h", "cvms", "cvmsi", "cencal", "cvmh", "albacore", "cvlsu", "ivlsu", "wfcvm", "labasin"]
 
 cd $UCVM_SRC_PATH/largefiles
 ./get_largefiles.py << EOF
@@ -15,6 +15,7 @@ y
 n
 n
 y
+n
 n
 n
 n
