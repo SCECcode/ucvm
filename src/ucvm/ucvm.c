4cfa25f0d81b6dbcfd6ce00b260618260d348041
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2022-03-17 06:05:45 +0000

    remove extra cvms

--- src/ucvm/ucvm.c
+++ src/ucvm/ucvm.c
@@ -1093,15 +1093,6 @@ int ucvm_get_resources(ucvm_resource_t *res, int *len)
     return(UCVM_CODE_ERROR);
   }
 
-
-#ifdef _UCVM_ENABLE_CVMS
-  if (ucvm_save_resource(UCVM_RESOURCE_MODEL, UCVM_MODEL_CRUSTAL,
-		     UCVM_MODEL_CVMS, "", res, numinst++, *len) 
-      != UCVM_CODE_SUCCESS) {
-    return(UCVM_CODE_ERROR);
-  }
-#endif
-
 #ifdef _UCVM_ENABLE_CVMH
   if (ucvm_save_resource(UCVM_RESOURCE_MODEL, UCVM_MODEL_CRUSTAL,
 		     UCVM_MODEL_CVMH, "", res, numinst++, *len) 
