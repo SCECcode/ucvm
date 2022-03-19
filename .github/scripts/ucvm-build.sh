fd880d125dc6566af04f84c55df2f87a352956bc
Mei-Hui Su <mei@MeiPro.local>
2022-03-19 14:48:23 -0700

    generalize the actions

---
+++ .github/scripts/ucvm-build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+tmp=`uname -s`
+
+MODEL=$1
+
+if [ "$MODEL" == "" ]; then
+   echo "FAIL: needs to supply a model name .."
+   exit 1
+fi
+
+if [ $tmp == 'Darwin' ]; then
+## make sure have automake/aclocal
+  brew install automake
+fi
+
+mkdir $UCVM_INSTALL_PATH
+
+##["cvms5", "cca", "cs173", "cs173h", "cvms", "cvmsi", "cencal", "cvmh", "albacore", "cvlsu", "ivlsu", "wfcvm","cvmhlabn"]
+
+cd $UCVM_SRC_PATH/largefiles
+./get_largefiles.py -m $MODEL
+
+if [ $tmp != 'Darwin' ]; then
+  cd $UCVM_SRC_PATH/largefiles; ./check_largefiles_md5.py
+fi
+cd $UCVM_SRC_PATH/largefiles; ./stage_largefiles.py
+
+cd $UCVM_SRC_PATH
+./ucvm_setup.py -d -a -p $UCVM_INSTALL_PATH  &> ucvm_setup_install.log
+
+#more ucvm_setup_install.log
+
