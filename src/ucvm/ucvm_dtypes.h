b86be216dfb450de11a09a215b5f080debd6d82a
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2023-11-29 05:05:08 +0000

    wip  adding ucvm_model_dtypes.h

--- src/ucvm/ucvm_dtypes.h
+++ src/ucvm/ucvm_dtypes.h
@@ -3,6 +3,8 @@
 
 #include <stdarg.h>
 
+#include "ucvm_model_dtypes.h"
+
 /* Maximum number of supported models */
 #define UCVM_MAX_MODELS 30
 
@@ -134,17 +136,47 @@ typedef enum { UCVM_BYTEORDER_LSB = 0,
                UCVM_BYTEORDER_MSB } ucvm_byteorder_t;
 
 
+/* XX */
+
 /* Supported error codes */
 typedef enum { UCVM_CODE_SUCCESS = 0, 
-	       UCVM_CODE_ERROR,
-	       UCVM_CODE_DATAGAP,
-	       UCVM_CODE_NODATA } ucvm_code_t;
+	       UCVM_CODE_ERROR = 1,
+	       UCVM_CODE_NODATA = 2,
+	       UCVM_CODE_DATAGAP = 3 } ucvm_code_t;
 
+/* Supported UCVM parameters visible to user. Argument list:
+UCVM_PARAM_QUERY_MODE    : ucvm_ctype_t qmode
+UCVM_PARAM_IFUNC_ZRANGE  : double min, double max (meters)
+UCVM_PARAM_MODEL_CONF    : char *mlabel, char *param, char *value
+  CVM-H:
+    "USE_1D_BKG": "True"/"False"
+
+index takes in account for ucvm_mparam_t
+*/
+typedef enum { UCVM_PARAM_QUERY_MODE = 2,
+	       UCVM_PARAM_IFUNC_ZRANGE = 3,
+	       UCVM_PARAM_MODEL_CONF = 4 } ucvm_param_t;
+
+
+/* Supported model parameters. Used internally by UCVM 
+
+UCVM_MODEL_PARAM_FORCE_DEPTH_ABOVE_SURF : Force elevation points to be
+  treated as offset relative to regional model surface when it is 
+  above the ucvm dem surface. Removes discontinuities between model 
+  surface and GTL.
+
+UCVM_MODEL_PARAM_PLUGIN_MODE : Tell the backend model that it is being 
+  invoked as plugin module and is always expected to be 'query by depth'
+
+*/
+typedef enum { UCVM_MODEL_PARAM_FORCE_DEPTH_ABOVE_SURF = 0,
+               UCVM_MODEL_PARAM_PLUGIN_MODE = 1} ucvm_mparam_t;
 
 /* Supported coordinate query modes */
 typedef enum { UCVM_COORD_GEO_DEPTH = 0, 
-	       UCVM_COORD_GEO_ELEV } ucvm_ctype_t;
+	       UCVM_COORD_GEO_ELEV =1 } ucvm_ctype_t;
 
+/* XX */
 
 /* Supported model types */
 typedef enum { UCVM_MODEL_CRUSTAL = 0, 
@@ -157,31 +189,6 @@ typedef enum { UCVM_RESOURCE_MODEL = 0,
 	       UCVM_RESOURCE_MAP,
 	       UCVM_RESOURCE_MODEL_IF,
 	       UCVM_RESOURCE_MAP_IF} ucvm_rtype_t;
-
-
-/* Supported UCVM parameters visible to user. Argument list:
-
-UCVM_PARAM_QUERY_MODE    : ucvm_ctype_t qmode
-UCVM_PARAM_IFUNC_ZRANGE  : double min, double max (meters)
-UCVM_PARAM_MODEL_CONF    : char *mlabel, char *param, char *value
-  CVM-H:
-    "USE_1D_BKG": "True"/"False"
-
-*/
-typedef enum { UCVM_PARAM_QUERY_MODE = 1,
-	       UCVM_PARAM_IFUNC_ZRANGE = 2,
-	       UCVM_PARAM_MODEL_CONF = 3 } ucvm_param_t;
-
-
-/* Supported model parameters. Used internally by UCVM 
-
-UCVM_MODEL_PARAM_FORCE_DEPTH_ABOVE_SURF : Force elevation points to be
-  treated as offset relative to regional model surface when it is 
-  above the ucvm dem surface. Removes discontinuities between model 
-  surface and GTL.
-
-*/
-typedef enum { UCVM_MODEL_PARAM_FORCE_DEPTH_ABOVE_SURF = 0 } ucvm_mparam_t;
 
 
 /* Supported coordinate query modes. Used internally by UCVM */
---
+++ src/ucvm/ucvm_model_dtypes.h
@@ -0,0 +1,43 @@
+#ifndef UCVM_MODEL_DTYPES_H
+#define UCVM_MODEL_DTYPES_H
+
+// ucvm model specific dtypes.h
+
+// from ucvm's src/ucvm/ucvm_dtypes.h
+/* Supported error codes  */
+typedef enum { UCVM_MODEL_CODE_SUCCESS = 0,
+               UCVM_MODEL_CODE_ERROR = 1,
+               UCVM_MODEL_CODE_NODATA = 2 } ucvm_model_code_t;
+
+/* Supported UCVM parameters visible to user. Argument list:
+UCVM_PARAM_QUERY_MODE    : ucvm_ctype_t qmode
+UCVM_PARAM_IFUNC_ZRANGE  : double min, double max (meters)
+UCVM_PARAM_MODEL_CONF    : char *mlabel, char *param, char *value
+  CVM-H:
+    "USE_1D_BKG": "True"/"False"
+indexing takes in account for ucvm_model_mparam_t
+*/
+
+typedef enum { UCVM_MODEL_PARAM_QUERY_MODE = 1,
+               UCVM_MODEL_PARAM_IFUNC_ZRANGE = 2,
+               UCVM_MODEL_PARAM_MODEL_CONF = 3 } ucvm_model_param_t;
+
+/* Supported model parameters. Used internally by UCVM
+UCVM_MODEL_PARAM_FORCE_DEPTH_ABOVE_SURF : Force elevation points to be
+  treated as offset relative to regional model surface when it is
+  above the ucvm dem surface. Removes discontinuities between model
+  surface and GTL.
+UCVM_MODEL_PARAM_PLUGIN_MODE : Tell the backend model that it is being
+  invoked as plugin module and is always expected to be 'query by depth'
+
+*/
+typedef enum { UCVM_MODEL_PARAM_FORCE_DEPTH_ABOVE_SURF = 0,
+               UCVM_MODEL_PARAM_PLUGIN_MODE = 1 } ucvm_model_mparam_t;
+
+/* Supported coordinate query modes */
+typedef enum { UCVM_MODEL_COORD_GEO_DEPTH = 0,
+               UCVM_MODEL_COORD_GEO_ELEV = 1 } ucvm_model_ctype_t;
+
+#endif
+
+
