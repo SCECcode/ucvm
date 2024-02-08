69c099078b92044f960efafca7f862df1f282032
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2024-02-08 22:52:11 +0000

    fleshing out cs242's ucvm updates

--- src/ucvm/ucvm_model_plugin.c
+++ src/ucvm/ucvm_model_plugin.c
@@ -61,6 +61,13 @@
      extern int cca_version;
      extern int cca_config;
 #endif
+#ifdef _UCVM_ENABLE_CS242
+     extern int cs242_init;
+     extern int cs242_query;
+     extern int cs242_finalize;
+     extern int cs242_version;
+     extern int cs242_config;
+#endif
 #ifdef _UCVM_ENABLE_CVMHLABN
      extern int cvmhlabn_init;
      extern int cvmhlabn_query;
@@ -347,6 +354,19 @@ int ucvm_plugin_model_init(int id, ucvm_modelconf_t *conf) {
                 pptr->model_finalize = &cca_finalize;
                 pptr->model_version = &cca_version;
                 pptr->model_config = &cca_config;
+                if ((*pptr->model_init)(conf->config, conf->label) != 0) {
+                        fprintf(stderr, "Failed to initialize model, %s.\n", conf->label);
+                        return UCVM_CODE_ERROR;
+                }
+        }
+#endif
+#ifdef _UCVM_ENABLE_CS242
+        if (strcmp(conf->label, UCVM_MODEL_CS242) == 0) {
+                pptr->model_init = &cs242_init;
+                pptr->model_query = &cs242_query;
+                pptr->model_finalize = &cs242_finalize;
+                pptr->model_version = &cs242_version;
+                pptr->model_config = &cs242_config;
                 if ((*pptr->model_init)(conf->config, conf->label) != 0) {
                         fprintf(stderr, "Failed to initialize model, %s.\n", conf->label);
                         return UCVM_CODE_ERROR;
