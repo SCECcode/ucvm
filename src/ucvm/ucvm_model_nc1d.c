c5c5b68b3fbe300a09167d58ccb806ee053816c5
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2024-04-25 20:34:25 +0000

    fix collision

--- src/ucvm/ucvm_model_nc1d.c
+++ src/ucvm/ucvm_model_nc1d.c
@@ -35,7 +35,7 @@ ucvm_modelconf_t ucvm_nc1d_conf;
 ucvm_config_t *ucvm_nc1d_cfg = NULL;
 
 
-ucvm_nc1d_interpolation_t interptype;
+ucvm_nc1d_interpolation_t ucvm_nc1d_interptype;
 
 double ucvm_nc1d_vp(double depth) {
 	int i;
@@ -53,7 +53,7 @@ double ucvm_nc1d_vp(double depth) {
 	// Find out which layer we're in.
 	for (i = 0; i < ucvm_nc1d_z_dim; i++) {
 		if (cumulativeDepth + ucvm_nc1d_layer_depths[i] > depth) {
-			if (interptype == nc1d_NONE || i == 0) {
+			if (ucvm_nc1d_interptype == nc1d_NONE || i == 0) {
 				vp = ucvm_nc1d_layer_vp[i];
 			} else {
 		        depth_ratio = (depth - cumulativeDepth) / ucvm_nc1d_layer_depths[i];
@@ -91,7 +91,7 @@ double ucvm_nc1d_vs(double depth) {
 	// Find out which layer we're in.
 	for (i = 0; i < ucvm_nc1d_z_dim; i++) {
 		if (cumulativeDepth + ucvm_nc1d_layer_depths[i] > depth) {
-			if (interptype == nc1d_NONE || i == 0) {
+			if (ucvm_nc1d_interptype == nc1d_NONE || i == 0) {
 				vs = ucvm_nc1d_layer_vs[i];
 			} else {
 		        depth_ratio = (depth - cumulativeDepth) / ucvm_nc1d_layer_depths[i];
@@ -129,7 +129,7 @@ double ucvm_nc1d_rho(double depth) {
 	// Find out which layer we're in.
 	for (i = 0; i < ucvm_nc1d_z_dim; i++) {
 		if (cumulativeDepth + ucvm_nc1d_layer_depths[i] > depth) {
-			if (interptype == nc1d_NONE || i == 0) {
+			if (ucvm_nc1d_interptype == nc1d_NONE || i == 0) {
 				rho = ucvm_nc1d_layer_rho[i];
 			} else {
 		        depth_ratio = (depth - cumulativeDepth) / ucvm_nc1d_layer_depths[i];
@@ -217,9 +217,9 @@ int ucvm_nc1d_model_init(int m, ucvm_modelconf_t *conf)
   }
 
   if (strcmp(cptr->value, "none") == 0) {
-	  interptype = nc1d_NONE;
+	  ucvm_nc1d_interptype = nc1d_NONE;
   } else if (strcmp(cptr->value, "linear") == 0) {
-	  interptype = nc1d_LINEAR;
+	  ucvm_nc1d_interptype = nc1d_LINEAR;
   } else {
 	  fprintf(stderr, "Invalid interpolation method defined\n");
 	  return 1;
