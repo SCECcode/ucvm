da95f57a57c1798089942815b01a797d188f32ca
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2025-02-11 17:22:19 +0000

    add sjfz into ucvm

--- src/ucvm/ucvm.c
+++ src/ucvm/ucvm.c
@@ -1334,6 +1334,13 @@ int ucvm_get_resources(ucvm_resource_t *res, int *len)
     return(UCVM_CODE_ERROR);
   }
 #endif
+#ifdef _UCVM_ENABLE_SJFZ
+  if (ucvm_save_resource(UCVM_RESOURCE_MODEL, UCVM_MODEL_CRUSTAL,
+                     UCVM_MODEL_SJFZ, "", res, numinst++, *len)
+      != UCVM_CODE_SUCCESS) {
+    return(UCVM_CODE_ERROR);
+  }
+#endif
 #ifdef _UCVM_ENABLE_CS248
   if (ucvm_save_resource(UCVM_RESOURCE_MODEL, UCVM_MODEL_CRUSTAL,
                      UCVM_MODEL_CS248, "", res, numinst++, *len)
