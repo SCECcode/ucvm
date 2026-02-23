0e3ce56d7c7e007e3ca8b63d8864896a0c52eef0
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2026-02-23 18:38:26 +0000

    wip

--- src/ucvm/ucvm.c
+++ src/ucvm/ucvm.c
@@ -1261,6 +1261,13 @@ int ucvm_get_resources(ucvm_resource_t *res, int *len)
     return(UCVM_CODE_ERROR);
   }
 #endif
+#ifdef _UCVM_ENABLE_UWPKFCVM
+  if (ucvm_save_resource(UCVM_RESOURCE_MODEL, UCVM_MODEL_CRUSTAL,
+                     UCVM_MODEL_UWPKFCVM, "", res, numinst++, *len)
+      != UCVM_CODE_SUCCESS) {
+    return(UCVM_CODE_ERROR);
+  }
+#endif
 #ifdef _UCVM_ENABLE_CVMH
   if (ucvm_save_resource(UCVM_RESOURCE_MODEL, UCVM_MODEL_CRUSTAL,
                      UCVM_MODEL_CVMH, "", res, numinst++, *len)
