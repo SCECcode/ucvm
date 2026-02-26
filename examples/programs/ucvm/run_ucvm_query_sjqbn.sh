ed9cd2f63b62535ad52ebf687c6b6c0591b4a58d
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2026-02-26 07:50:37 +0000

    add sjqbn test

---
+++ examples/programs/ucvm/run_ucvm_query_sjqbn.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ -z "$UCVM_INSTALL_PATH" ]; then
+  echo "Need to set UCVM_INSTALL_PATH to run >" ${0##*/} 
+  exit
+fi
+source $UCVM_INSTALL_PATH/conf/ucvm_env.sh
+
+BIN_DIR=${UCVM_INSTALL_PATH}/bin
+CONF_DIR=${UCVM_INSTALL_PATH}/conf
+TEST_DIR=${UCVM_INSTALL_PATH}/tests/inputs
+MODEL=sjqbn
+
+expect=$(mktemp) || exit 1
+result=$(mktemp) || (trap 'rm -f "$expect"'; exit 1)
+
+${BIN_DIR}/ucvm_query -m ${MODEL} -f ${CONF_DIR}/ucvm.conf < ${TEST_DIR}/test_latlons_sjqbn_gd.txt  > $result 2>&1
+
+cat > $expect << EOF_EXPECTED_RESULT
+EOF_EXPECTED_RESULT
+
+echo "Running examples_programs_ucvm ucvm_query_sjqbn"
+if diff $result $expect > /dev/null 2>&1
+then
+  echo [SUCCESS]
+else
+  echo [FAILURE]
+fi
+
+trap 'rm -f "$expect" "$result"' exit
+
